843e3a2135e9525322927d8d06238f52ac5fd10c
anmoldeep1512 <anmoldeep1512@gmail.com>
2023-12-28 17:45:22 +0530

    added basic code for using openai to built chatbot

--- backend.py
+++ backend.py
@@ -1,2 +1,21 @@
+import openai
+
+
 class Chatbot:
-    pass
+    def __init__(self):
+        openai.api_key = ''
+
+    def get_response(self, user_input):
+        response = openai.ChatCompletion.create(
+            engine='text-davinci-003',
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+
+if __name__ == '__main__':
+    chatbot = Chatbot()
+    response = chatbot.get_response('Write a joke about birds')
+    print(response)
